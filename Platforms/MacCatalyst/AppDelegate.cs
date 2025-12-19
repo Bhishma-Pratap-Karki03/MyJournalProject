4dc402ad77c0c0e79d88d37065e02f77de3eb862
bhishma-pratap-karki <bhishmapratapkarki05@gmail.com>
2025-12-19 14:02:27 +0545

    Initial commit - MyJournalProject

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyJournalProject
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
