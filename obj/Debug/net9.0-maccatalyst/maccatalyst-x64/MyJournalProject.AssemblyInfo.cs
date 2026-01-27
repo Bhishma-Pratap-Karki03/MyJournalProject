b88cd9548d443ec554832f94b7d85380541ca3cd
bhishma-pratap-karki <bhishmapratapkarki05@gmail.com>
2026-01-27 13:47:11 +0545

    Updated and Final Code

--- obj/Debug/net9.0-maccatalyst/maccatalyst-x64/MyJournalProject.AssemblyInfo.cs
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-x64/MyJournalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyJournalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b25c4f40855bfffa047a9f3d765acd0a52417984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7ca67bf1c0acab7c85931db1c6b621a0d3cea028")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyJournalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyJournalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
